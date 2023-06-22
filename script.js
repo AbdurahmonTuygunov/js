8361afd3147bb92f8695d7b4313bfb3b84a3435a
AbdurahmonTuygunov <136953925+AbdurahmonTuygunov@users.noreply.github.com>
2023-06-22 17:25:39 +0500

    js
    js

---
+++ script.js
@@ -0,0 +1,67 @@
+var modal = prompt("Ismingizni kiriting")
+console.log("Mening ismim " + modal);
+
+var age = prompt("Yoshingizni kiriting")
+console.log("Men " + age + " yoshdaman");
+
+var task = prompt("Misolni yeching 6 + 5 = ?")
+console.log("Misol 1: 6 + 5 = " + (6 + 5) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 95 + 45 = ?")
+console.log("Misol 2: 95 + 45 = " + (95 + 45) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 95 * 45 = ?")
+console.log("Misol 3: 95 * 45 = " + (95 * 45) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 95 / 45 = ?")
+console.log("Misol 4: 95 / 45 = " + (95 / 45) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 55 + 405 = ?")
+console.log("Misol 5: 55 + 405 = " + (55 + 405) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 55 * 405 = ?")
+console.log("Misol 6: 55 * 405 = " + (55 * 405) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 55 / 405 = ?")
+console.log("Misol 7: 55 / 405 = " + (55 / 405) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 95 - 45 = ?")
+console.log("Misol 8: 95 - 45 = " + (95 - 45) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 10 + 10 = ?")
+console.log("Misol 9: 10 + 10 = " + (10 + 10) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 20 - 15.5 = ?")
+console.log("Misol 10: 20 - 15.5 = " + (20 - 15.5) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 5 * 5 = ?")
+console.log("Misol 11: 5 * 5 = " + (5 * 5) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 5 / 5 = ?")
+console.log("Misol 12: 5 / 5 = " + (5 / 5) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 238237 + 837829 = ?")
+console.log("Misol 13: 238237 + 837829 = " + (238237 + 837829) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 238362 - 1 = ?")
+console.log("Misol 14: 238362 - 1 = " + (238362 - 1) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 5 * 45 = ?")
+console.log("Misol 15: 5 * 45 = " + (5 * 45) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 1000 / 100 = ?")
+console.log("Misol 16: 1000 / 100 = " + (1000 / 100) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 54 + 45 = ?")
+console.log("Misol 17: 54 + 45 = " + (54 + 45) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 238947 - 6798 = ?")
+console.log("Misol 18: 238947 - 6798 = " + (238947 - 6798) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 2345 * 980 = ?")
+console.log("Misol 19: 2345 * 980 = " + (2345 * 980) + " Sizning javobingiz " + task);
+
+var task = prompt("Misolni yeching 456456 / 456 = ?")
+console.log("Misol 20: 456456 / 456 = " + (456456 / 456) + " Sizning javobingiz " + task);
+
+var result = alert("Natijani ko'rish uchun konsolni oching")
