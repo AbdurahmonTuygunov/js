cb320d9a0fc03b67ab0e4c0e629eeebf4d5a1342
AbdurahmonTuygunov <136953925+AbdurahmonTuygunov@users.noreply.github.com>
2023-06-29 17:49:16 +0500

    Update script.js
    Update

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ if (task==6/5) {
 }
 
 alert("Natijani ko'rish uchun konsolni oching")
+
+alert("KEYINGI UY ISHI")
+
+var name = prompt("Yoshingizni kiriting")
+do {
+    var name = +prompt("Nimadir noto'g'ri ketib qoldi")
+} while (isNaN(name));
+if (name <= 18) {
+    alert("Yoshsiz, oqishingiz kerak");
+}else if (name <= 50) {
+    alert("Ishlashingiz kerak");
+}else if(name <= 59) {
+    alert("Yaqinda pensiyaga chiqasiz");
+}else if(name <= 150) {
+    alert("Pensionerga o'xshaysiz, hali tirik bo'lsangiz :)");
+}else if(name >= 150) {
+    alert("Nimadir noto'g'ri ketib qoldi")
+}
