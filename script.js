1ceaf870189fa8917f4762ac84d35143f261a146
AbdurahmonTuygunov <136953925+AbdurahmonTuygunov@users.noreply.github.com>
2023-07-02 10:46:45 +0500

    Update script.js
    Update

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ if( num1 > num2 && num1 < num3 || num1 < num2 && num3 < num1 ){
   
 }else if( num3 > num2 && num2 > num1 || num3 > num2 && num2 > num1) {
 
-}else if (num1 > num2 && num2 < num3) {
+}else if (num2 > num3 && num3 > num1 || num1 > num3 && num3 > num2) {
   alert("O'rtacha qiymat " + num3)
 }
 else{
